7108af8bc96547167265e4991945368e737cd782
snowmerak <snowmerak@outlook.com>
2024-04-07 01:57:46 +0900

    add port option

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/snowmerak/regulus/regulus"
+import (
+	"os"
+
+	"github.com/snowmerak/regulus/regulus"
+)
 
 func main() {
-	server := regulus.New(":8080")
+	if len(os.Args) < 2 {
+		panic("Usage: regulus <port>")
+	}
+
+	port := os.Args[1]
+	if port == "" {
+		port = ":8080"
+	}
+
+	server := regulus.New(port)
 
 	if err := server.Start(); err != nil {
 		panic(err)
